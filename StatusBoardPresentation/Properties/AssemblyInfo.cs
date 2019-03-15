330ba4497bacc80a93c7271271223e5d4e37b999
dsbjax <dsbolton@comcast.net>
2019-03-15 11:23:12 -0400

    Reboot

--- StatusBoardPresentation/Properties/AssemblyInfo.cs
+++ StatusBoardPresentation/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("StausBoardDatabase")]
+[assembly: AssemblyTitle("Status Board Presentation Layer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("StausBoardDatabase")]
+[assembly: AssemblyCompany("KBR Wyle")]
+[assembly: AssemblyProduct("Status Board")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.2019.74")]
-[assembly: AssemblyFileVersion("1.0.2019.74")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- StatusBoardPresentation/Properties/Resources.Designer.cs
+++ StatusBoardPresentation/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace StausBoardDatabase.Properties {
+namespace StatusBoardPresentation.Properties {
     
     
     /// <summary>
@@ -38,7 +38,7 @@ namespace StausBoardDatabase.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if ((resourceMan == null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("StausBoardDatabase.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("StatusBoardPresentation.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
